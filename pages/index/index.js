08c0c19c997d1db29a11436b9fcfb79ddf245094
ljc <315324144@qq.com>
2018-09-13 23:36:47 +0800

    18-09-13-last-ljc

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,8 @@
 // pages/index/index.js
 const app=getApp();
 const appdata = app.globalData;
+const url = appdata.app_address + appdata.subDomain;
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -14,11 +14,45 @@ Page({
     imgUrls: null,
     categrory:null,
     navOn:0,
+    discounts:null,
+    goodsList:null,
+    notice:null,
   },
+  // 点击衣服种类导航 
   changeCategrory:function(e){
+    var that=this;
+    let categoryId= e.currentTarget.id;
     this.setData({
       navOn: e.currentTarget.id
     })
+    wx.request({
+      url: url + '/shop/goods/list',
+      data: { categoryId: categoryId},
+      success:function(res){
+        // that.setData({
+        //   goodsList: null
+        // })
+        if (res.data.code==0){
+          console.log(222);
+          that.setData({
+            goodsList: res.data.data
+          })
+        }else{
+          that.setData({
+            goodsList:null
+          })
+        }
+      }
+    })
+  },
+  getDiscount:function(e){
+    console.log(e.currentTarget.id)
+  },
+  goShopDetail:function(e){
+    console.log(e.currentTarget.id)
+  },
+  goNoticeDeTail:function(e){
+    console.log(e.currentTarget.id)
   },
   /**
    * 生命周期函数--监听页面加载
@@ -45,7 +79,39 @@ Page({
         })
       }
     })
+    // 获取优惠券
+    wx.request({
+      url: url +'/discounts/coupons',
+      success:function(res){
+       that.setData({
+         discounts: res.data.data
+       })
+      }
+    })
+  // 获取商品列表
+  wx.request({
+    url: url +'/shop/goods/list',
+    success:function(res){
+      console.log(res);
+      that.setData({
+        goodsList:res.data.data
+      })
+    }
+  })
+  // 获取公告列表
+  wx.request({
+    url: url +'//notice/list',
+    success:function(res){
+      console.log(res);
+      if(res.data.code==0){
+        that.setData({
+          notice: res.data.data.dataList
+        })
+      }
+    }
+  })
   },
+  
 
   /**
    * 生命周期函数--监听页面初次渲染完成
