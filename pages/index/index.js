0b65a75a954b433a004818bd288adb0417927a04
李金城 <lijincheng@maysunmedia.com>
2018-10-09 17:12:49 +0800

    2018-10-09-last

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,11 +14,41 @@ Page({
     imgUrls: null,
     categrory:null,
     navOn:0,
+    shopList:null,
+    coupons:null,
+    hideCoupons:true,
   },
-  changeCategrory:function(e){
-    this.setData({
-      navOn: e.currentTarget.id
+  // 获取商品
+  getshopList: function () {
+    let this_=this;
+    wx.request({
+      url: appdata.app_address + appdata.subDomain + '/shop/goods/list',
+      data:{
+        categoryId:this_.data.navOn
+      },
+      success(res){
+        if(res.data.code==0){
+          this_.setData({
+            shopList:res.data.data
+          })
+        }else{
+          this_.setData({
+            shopList: ''
+          })
+        }
+      }
     })
+  },
+  // 分类切换
+  changeCategrory:function(e){
+    let categoryId = e.currentTarget.id
+    this.setData({
+      navOn: categoryId
+    })
+    this.getshopList()
+  },
+  tabCoupons:function(e){
+    console.log(e.currentTarget.id)
   },
   /**
    * 生命周期函数--监听页面加载
@@ -44,9 +74,26 @@ Page({
           categrory: res.data.data
         })
       }
+    }),
+    // 获取优惠券
+    wx.request({
+      url: appdata.app_address + appdata.subDomain + '/discounts/coupons',
+      success:function(res){
+        if(res.data.code!=0 || res.data.data.length==0){
+          that.setData({
+            hideCoupons:false
+          })
+        }else{
+          that.setData({
+            hideCoupons: false,
+            coupons:res.data.data
+          })
+        }
+      }
     })
+   this.getshopList()
   },
-
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
