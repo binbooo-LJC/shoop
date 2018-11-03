4006cdc470f1c9a3c10cc04967101924a8658085
李金城 <lijincheng@maysunmedia.com>
2018-11-03 11:16:26 +0800

    2018-11-3-123

--- pages/order/index.js
+++ pages/order/index.js
@@ -19,33 +19,33 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-      var that=this;
-      // 获取购买信息
-      var buyInfo=wx.getStorage({
-        key: 'buyinfo',
-        success: function(res) {
-          console.log(res.data.list.length)
-          if (res.data.list.length>0){
-            that.setData({
-              buyInfo:res.data.list
-            })
-          }
-        },
-      })
+      // var that=this;
+      // // 获取购买信息
+      // var buyInfo=wx.getStorage({
+      //   key: 'buyinfo',
+      //   success: function(res) {
+      //     console.log(res.data.list.length)
+      //     if (res.data.list.length>0){
+      //       that.setData({
+      //         buyInfo:res.data.list
+      //       })
+      //     }
+      //   },
+      // })
       // 检查用户是否存在地址
-      wx.request({
-        url: url +'/user/shipping-address/list',
-        data:{token:wx.getStorageSync('token')},
-        success:function(res){
-          if(res.data.code==700){
-            wx.showModal({
-              title: '提示',
-              content: '请添加地址',
-              showCancel:false
-            })
-          }
-        }
-      })
+      // wx.request({
+      //   url: url +'/user/shipping-address/list',
+      //   data:{token:wx.getStorageSync('token')},
+      //   success:function(res){
+      //     if(res.data.code==700){
+      //       wx.showModal({
+      //         title: '提示',
+      //         content: '请添加地址',
+      //         showCancel:false
+      //       })
+      //     }
+      //   }
+      // })
   },
 
 
@@ -60,7 +60,33 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    var that = this;
+    // 获取购买信息
+    var buyInfo = wx.getStorage({
+      key: 'buyinfo',
+      success: function (res) {
+        console.log(res.data.list.length)
+        if (res.data.list.length > 0) {
+          that.setData({
+            buyInfo: res.data.list
+          })
+        }
+      },
+    })
+    // 检查用户是否存在地址
+    wx.request({
+      url: url + '/user/shipping-address/default',
+      data: { token: wx.getStorageSync('token') },
+      success: function (res) {
+        if (res.data.code !=0) {
+          wx.showModal({
+            title: '错误',
+            content: '请选择默认地址',
+            showCancel: false
+          })
+        }
+      }
+    })
   },
 
   /**
