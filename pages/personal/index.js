ac828c8fddef7e3c46af64d04bd0806e614d3ddf
李金城 <lijincheng@maysunmedia.com>
2018-11-08 18:05:04 +0800

    2018-11--ljc-last

--- pages/personal/index.js
+++ pages/personal/index.js
@@ -1,4 +1,7 @@
 // pages/personal/index.js
+const app = getApp();
+const appdata = app.globalData;
+const url = appdata.app_address + appdata.subDomain;
 Page({
 
   /**
@@ -7,7 +10,48 @@ Page({
   data: {
 
   },
-
+  goqiandao:function(e){
+    console.log(e.detail.formId)
+   wx.request({
+     url: url +'/template-msg/put',
+     data:{
+       token:wx.getStorageSync('token'),
+       type:0,
+       module:'immediately',
+       template_id:'ll1m0tXwx1Y7Uxg_X7_YrtPOk06PsxLoV4jhiFt8xRo',
+       form_id: e.detail.formId,
+       url:'pages/personal/index',
+       postJsonString:{
+         "keyword1": {
+           "value": "李金城",
+           "color": "#173177"
+         },
+         "keyword2": {
+           "value": "2015年01月05日 12:30",
+           "color": "#173177"
+         },
+         "keyword3": {
+           "value": "粤海喜来登酒店",
+           "color": "#173177"
+         },
+         "keyword4": {
+           "value": "2017-11-12 12:00",
+           "color": "#173177"
+         },
+         "keyword5": {
+           "value": "感谢您的参加",
+           "color": "#173177"
+         },
+         "keyword6": {
+           "value": "13366659939",
+           "color": "#173177"
+         }
+       }
+     },success:function(res){
+       console.log(res);
+     }
+   })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
